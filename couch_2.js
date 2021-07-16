bfb3b9ea56f08983140863a5e53317deb28a9d38
snomkip7 <73748270+snomkip7@users.noreply.github.com>
2021-07-16 16:04:10 -0400

    Add files via upload

---
+++ couch_2.js
@@ -0,0 +1,60 @@
+
+img = "";
+status = "";
+x = "";
+y = "";
+width = "";
+height = "";
+objects = [];
+
+function preload(){
+    img = loadImage("couch_2.jpg");
+
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded :)");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+
+    if(status != ""){
+        for(i=0;i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            x = objects[i].x;
+            
+            y = objects[i].y;
+            
+            width = objects[i].width;
+            
+            height = objects[i].height;
+           
+
+            fill("#0000ff");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " "+ percent + "%", x+10, y+10);
+            noFill();
+            stroke("#0000ff");
+            rect(x, y, width, height);
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,25 @@
+
+img = "";
+
+function preload(){
+    //img = loadImage("dog_cat.jpg");
+    //img = loadImage("image_1.jpg");
+}
+
+//function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+//}
+
+//function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    //text("Dog", 45, 75);
+    //text("Cheetah", 160, 25);
+    //text("Dog", 330, 65);
+    noFill();
+    stroke("#FF0000");
+    //rect(30, 60, 450, 350);
+    //rect(150, 10, 170, 400);
+    //rect(300, 40, 230, 375);
+//}
